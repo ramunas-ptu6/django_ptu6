31c9075cc07e209d6cc83b9ceb11bfdc3458a0e9
Ramunas Pl <ramunas.platukis@yahoo.com>
2023-01-11 16:27:02 +0200

    Revert "pakeistas views.py failas"
    This reverts commit 61ce2a494a0083d0e6e5c8f16c940fa3bad513de.

--- mysite/library/views.py
+++ mysite/library/views.py
@@ -4,6 +4,4 @@ from django.http import HttpResponse
 def index(request):
     return HttpResponse("Labas, pasauli!")
 
-def index2(request):
-    return HttpResponse("Labas, pasauli!")
 # Create your views here.
