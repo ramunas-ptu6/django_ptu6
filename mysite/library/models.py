73b21acbc7203f2a949e759ce84685e1822cb444
Ramunas Pl <ramunas.platukis@yahoo.com>
2023-01-12 09:17:15 +0200

    sukurtas genre modelis

--- mysite/library/models.py
+++ mysite/library/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Genre(models.Model):
+    name = models.CharField('Pavadinimas', max_length=200, help_text='Įveskite knygos žanrą')
+
+    def __str__(self):
+        return self.name
