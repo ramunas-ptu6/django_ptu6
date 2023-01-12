0cfd93bc7653f55b9ee37984a63ec3bf91c19ce5
Ramunas Pl <ramunas.platukis@yahoo.com>
2023-01-12 11:44:58 +0200

    sukurti pradiniai modeliai ir admin

--- mysite/library/admin.py
+++ mysite/library/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Author, Genre, Book, BookInstance
 
-# Register your models here.
+admin.site.register(Author)
+admin.site.register(Genre)
+admin.site.register(Book)
+admin.site.register(BookInstance)
--- mysite/library/models.py
+++ mysite/library/models.py
@@ -1,4 +1,8 @@
+import uuid
+
 from django.db import models
+from django.urls import reverse
+
 
 # Create your models here.
 class Genre(models.Model):
@@ -6,3 +10,61 @@ class Genre(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Book(models.Model):
+    title = models.CharField('Pavadinimas', max_length=200)
+    author = models.ForeignKey('Author', on_delete=models.SET_NULL, null=True)
+    summary = models.TextField('Aprašymas', max_length=1000, help_text='Trumpas knygos aprašymas')
+    isbn = models.CharField('ISBN', max_length=13)
+    genre = models.ManyToManyField(Genre, help_text='Išrinkite žanrą/us šiai knygai')
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('book-detail', args=[str(self.id)])
+
+
+class BookInstance(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, help_text='Unikalus knygos kopijos ID')
+    book = models.ForeignKey('Book', on_delete=models.SET_NULL, null=True)
+    due_back = models.DateField('Bus prieinama', null=True, blank=True)
+
+    LOAN_STATUS = (
+        ('a', 'Administruojama'),
+        ('p', 'Paimta'),
+        ('g', 'Galima paimti'),
+        ('r', 'Rezervuota')
+    )
+
+    status = models.CharField(
+        max_length=1,
+        choices=LOAN_STATUS,
+        blank=True,
+        default='a',
+        help_text='Status'
+    )
+
+    class Meta:
+        ordering = ['due_back']
+
+    def __str__(self):
+        return f'{self.id} {self.book.title}'
+
+
+class Author(models.Model):
+    first_name = models.CharField('Vardas', max_length=100)
+    last_name = models.CharField('Pavardė', max_length=100)
+
+    class Meta:
+        ordering = ['last_name', 'first_name']
+
+    def get_absolute_url(self):
+        return reverse('author-detail', args=[str(self.id)])
+
+    def __str__(self):
+        return f'{self.last_name}, {self.first_name}'
+
+class Car(models.Model):
+    nr = models.D
