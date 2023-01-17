0419578191104a32a4c61fe7e5f8075db968c819
Ramunas Pl <ramunas.platukis@yahoo.com>
2023-01-17 11:23:46 +0200

    5 paskaita, linkai autorių sąraše, Author pridėtas description laukas, patobulintas šablonas

--- mysite/library/models.py
+++ mysite/library/models.py
@@ -69,6 +69,7 @@ class BookInstance(models.Model):
 class Author(models.Model):
     first_name = models.CharField('Vardas', max_length=100)
     last_name = models.CharField('Pavardė', max_length=100)
+    description = models.TextField('Aprašymas', max_length=2000, default='bio')
 
     def display_books(self):
         return ', '.join([book.title for book in self.books.all()][:3]) + "..."
